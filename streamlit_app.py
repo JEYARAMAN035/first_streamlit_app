cb6568f7de7109d0225d265023f60d95f45e8657
JEYARAMAN035 <133550031+JEYARAMAN035@users.noreply.github.com>
2023-05-14 13:59:18 -0400

    Update streamlit_app.py
    changed header and added new dish

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit
 streamlit.title('My Parents New Healthy Dinner')
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Bluemerry Oatmeal')
-streamlit.text('Kale, spinach & Rocket smoothie')
-streamlit.text('Hard-Boiled Free-range Egg')
+streamlit.header('Breakfast Favourites')
+streamlit.text('🥣Omega 3 & Bluemerry Oatmeal')
+streamlit.text('🥗Kale, spinach & Rocket smoothie')
+streamlit.text('🐔Hard-Boiled Free-range Egg')
+streamlit.text('🥑🍞Hard-Boiled Free-range Egg')
