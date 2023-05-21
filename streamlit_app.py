405a1dbf3d903e3f158c452224548e4289759d6b
JEYARAMAN035 <133550031+JEYARAMAN035@users.noreply.github.com>
2023-05-21 16:52:25 -0400

    Update streamlit_app.py
    " removed at line def insert_row_snowflake(new_fruit"):

--- streamlit_app.py
+++ streamlit_app.py
@@ -64,7 +64,7 @@ streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_row)
 
 #allow the end user to add a fruit to the list
-def insert_row_snowflake(new_fruit"):    
+def insert_row_snowflake(new_fruit):    
     my_cnx.cursor() as my_cur:
         my_cur.execute("insert into PC_RIVERY_DB.PUBLIC.FRUIT_LOAD_LIST values ('from streamlit')")
         retrun "Thanks for adding " + new_fruit
